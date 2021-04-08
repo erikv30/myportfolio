41345b36dd14a1ea73e9dd36dec23b5a7d07b4d9
Erick Vega <erikvega30@hotmail.com>
2021-04-08 17:40:40 -0600

    portfolio revamp

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,45 +1,22 @@
-/* display menu */
-const overlay = document.getElementById('overlay')
-const closeMenu = document.getElementById('close-menu')
+/* Navbar */
+const navActive = () => {
+    const menu = document.querySelector('.menu-btn')
+    const nav = document.querySelector('.nav-links')
+    const navLinks = document.querySelectorAll('.nav-links li')
+    /* Toggle nav */
+    menu.addEventListener('click', () => {
+        nav.classList.toggle('nav-active')
 
-document.getElementById('open-menu').addEventListener('click', function() {
-    overlay.classList.add('show-menu')
-})
-
-document.getElementById('close-menu').addEventListener('click', function() {
-    overlay.classList.remove('show-menu')
-})
-
-/* .active  menu */
-const navLink = document.querySelectorAll('.nav-link')
-
-function linkAction() {
-    navLink.forEach(n => n.classList.remove('active'))
-    this.classList.add('active')
-
-    const navMenu = document.getElementById('overlay')
-    navMenu.classList.remove('show-menu')
+        /* Animated links */
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+            }
+        })
+    })
+    
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
-/*scroll reveal*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-})
-
-/* SR home */
-sr.reveal('.home-social-icon', {interval:200})
-
-/* SR about */
-sr.reveal('.about-img', {delay:200})
-
-/* SR skills */
-sr.reveal('.skill', {interval:200})
-
-/* SR projects */
-sr.reveal('.project-img', {interval:200})
-
+navActive()
